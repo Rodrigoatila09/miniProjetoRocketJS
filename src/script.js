301de928e75b2519ec2c73975de027e542281b7e
Rodrigoatila09 <154346800+Rodrigoatila09@users.noreply.github.com>
2025-03-15 01:24:43 -0300

    Ajustes em problemas relacionados exclusão de tarefas e criação

--- src/script.js
+++ src/script.js
@@ -1,3 +1,35 @@
-const inputUser = document.getElementById("userInput")
-const addItems = document.getElementById("addItems")
-const resultItems = document.getElementById("userItems")
+const inputUser = document.getElementById("userInput");
+const addItems = document.getElementById("addItems");
+const resultItems = document.getElementById("userItems");
+const arrayItems = [];
+
+addItems.addEventListener("click", () => {
+  sendValue();
+});
+
+function sendValue() {
+  arrayItems.push(inputUser.value);
+  renderItems();
+}
+
+function renderItems() {
+  resultItems.innerHTML = "";
+  arrayItems.forEach((item, index) => {
+    let newItem = document.createElement("p");
+    let btnDelete = document.createElement("i");
+    newItem.id = "pTag"
+
+    newItem.textContent = item; 
+    btnDelete.classList.add("fa-solid","fa-trash")
+
+    btnDelete.addEventListener("click", () => deleteItem(index));
+    
+    newItem.appendChild(btnDelete);
+    resultItems.appendChild(newItem);
+  });
+}
+
+function deleteItem(index) {
+  arrayItems.splice(index, 1);
+  renderItems();
+}
